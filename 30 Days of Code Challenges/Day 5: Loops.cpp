1773cae52cbd83e19564117f3e7c812c0b5b3b3c
Zachary J. Fields <zachary_fields@yahoo.com>
2016-04-10 20:06:47 -0700

    30 Days of Code Challenges - Day 5

---
+++ 30 Days of Code Challenges/Day 5: Loops.cpp
@@ -0,0 +1,35 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int N;
+    cin >> N;
+    for (int n = 1; n <= 10 ; ++n) {
+        cout << N << " x " << n << " = " << N * n << endl;
+    }
+    return 0;
+}
+
