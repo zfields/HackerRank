42cafcb447dc54838f82f9904838122ee880dcc9
Zachary J. Fields <zachary_fields@yahoo.com>
2016-04-10 20:00:35 -0700

    30 Days of Code Challenges - Day 4

---
+++ 30 Days of Code Challenges/Day 4: Class vs. Instance.cpp
@@ -0,0 +1,52 @@
+using namespace std;
+#include <iostream>
+
+class Person{
+public:
+   int age ;
+   Person(int initialAge);
+   void amIOld();
+   void yearPasses();
+};
+Person::Person(int initialAge){
+  // Add some more code to run some checks on initialAge
+    if ( initialAge >= 0 ) { age = initialAge; }
+    else {
+        age = 0;
+        cout << "Age is not valid, setting age to 0." << endl;
+    }
+}
+void Person::amIOld(){
+    // Do some computations in here and print out the correct statement to the console 
+    if ( age >= 18 ) { cout << "You are old." << endl; }
+    else if ( age >= 13 ) { cout << "You are a teenager." << endl; }
+    else { cout << "You are young." << endl; }
+}
+    
+
+void Person::yearPasses(){
+  // Increment the age of the person in here
+    ++age;
+}
+
+int main() {
+    int T, age;
+    cin>>T;
+    for(int i=0;i<T;i++)
+    {
+        cin>>age;
+        Person p(age);
+        p.AmIOld();
+        for(int j=0;j<3;j++)
+        {
+            p.yearPasses();
+
+        }
+        p.AmIOld();
+        cout<<"\n";
+    }
+    return 0;
+}
+
+
+
