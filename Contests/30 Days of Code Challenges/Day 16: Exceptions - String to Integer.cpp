7a558fa6c0e819b25ab301a1141303edd6b6f423
Zachary J. Fields <zachary_fields@yahoo.com>
2016-04-22 03:58:16 -0700

    30 Days of Code Challenges - Day 16

---
+++ Contests/30 Days of Code Challenges/Day 16: Exceptions - String to Integer.cpp
@@ -0,0 +1,39 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int64_t n;
+    string S;
+    cin >> S;
+    try {
+        n = stoi(S);
+        cout << n;
+    } catch (...) {
+        cout << "Bad String";
+    }
+    return 0;
+}
+
