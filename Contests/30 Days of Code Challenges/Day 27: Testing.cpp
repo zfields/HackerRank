6c67d1c1f07a6db6af78a8df7c175539b32aae66
Zachary J. Fields <zachary_fields@yahoo.com>
2016-04-26 05:49:15 -0700

    30 Days of Code Challenges - Day 27

---
+++ Contests/30 Days of Code Challenges/Day 27: Testing.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main () {
+    std::cout << "5" << std::endl;
+
+    std::cout << "7 5" << std::endl;
+    std::cout << "0 0 -3 -7 1 4 8" << std::endl;
+
+    std::cout << "3 2" << std::endl;
+    std::cout << "-1 2 0" << std::endl;
+
+    std::cout << "5 4" << std::endl;
+    std::cout << "0 2 -3 -1 8" << std::endl;
+
+    std::cout << "4 2" << std::endl;
+    std::cout << "0 2 -3 1" << std::endl;
+
+    std::cout << "6 5" << std::endl;
+    std::cout << "0 0 -3 1 2 -1" << std::endl;
+}
+
